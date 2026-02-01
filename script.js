c3d44dd73d10c93fde1e840622c2f24db26bc7de
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-02-02 01:38:11 +0800

    Added function createGrid(), added simple etch a sketch functionality

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const container = document.getElementById("main-container")
-const newDiv = document.createElement("div")
 
-let currentSize = 16
+function createGrid(rows, columns) {
+    area = rows * columns
 
-
-size = 16
-
-for (let c = 0; c <= size; c++) {
-    for (let r = 0; r <= size; r++) {
-        const newDiv = document.createElement("div")
+    for (let i = 0; i < area; i++){
+        const newDiv = document.createElement("div");
+        const text = document.createElement("p")
+        text.innerHTML = '0'
+        newDiv.append(text)
         newDiv.classList.add("myGrid")
         container.append(newDiv)
     }
 }
+
+const element = document.querySelectorAll("myGrid")
+
+for (let i = 0; i <= element.length; i++) {
+    let el = element[i]
+
+    el.onmouseenter = function () {
+        this.style.backgroundColor = 'white'
+    }
+}
+
+
+createGrid(3, 3)
