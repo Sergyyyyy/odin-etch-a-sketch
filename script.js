083f3087d49963f512f8c5cad1dd1bea0f866297
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-02-02 17:13:26 +0800

    Added basic etch-a-sketch functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.getElementById("main-container")
+const erase = document.getElementById("btn-erase-grid")
+const etch = document.getElementById("btn-etch-grid")
 
 function createGrid(rows, columns) {
     area = rows * columns
 
-    for (let i = 0; i < area; i++){
+    for (let i = 0; i < area; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("myGrid")
         container.append(newDiv)
@@ -12,11 +14,29 @@ function createGrid(rows, columns) {
 
 createGrid(3, 3)
 
-const element = document.querySelectorAll(".myGrid")
 
-for (let i = 0; i < element.length; i++){
-    let currentElement = element[i]
-    currentElement.onmouseenter = function(){
-        currentElement.style.backgroundColor = 'white'
+function toggleGrid() {
+    const element = document.querySelectorAll(".myGrid")
+    for (let i = 0; i < element.length; i++) {
+        let currentElement = element[i]
+        currentElement.onmouseenter = function () {
+            currentElement.style.backgroundColor = 'white'
+        }
     }
+}
+
+toggleGrid()
+
+erase.onclick = function () {
+    const element = document.querySelectorAll(".myGrid")
+    for (let i = 0; i < element.length; i++) {
+        let currentElement = element[i]
+        currentElement.onmouseenter = function () {
+            currentElement.style.backgroundColor = 'black'
+        }
+    }
+}
+
+etch.onclick = function () {
+    toggleGrid()
 }
