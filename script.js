1a747ede360a27741cd21edaab2a20f309c7af57
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-02-02 14:04:58 +0800

    Made the basic etch-a-sketch functionality with a 3x3 grid

--- script.js
+++ script.js
@@ -5,23 +5,18 @@ function createGrid(rows, columns) {
 
     for (let i = 0; i < area; i++){
         const newDiv = document.createElement("div");
-        const text = document.createElement("p")
-        text.innerHTML = '0'
-        newDiv.append(text)
         newDiv.classList.add("myGrid")
         container.append(newDiv)
     }
 }
 
-const element = document.querySelectorAll("myGrid")
+createGrid(3, 3)
 
-for (let i = 0; i <= element.length; i++) {
-    let el = element[i]
+const element = document.querySelectorAll(".myGrid")
 
-    el.onmouseenter = function () {
-        this.style.backgroundColor = 'white'
+for (let i = 0; i < element.length; i++){
+    let currentElement = element[i]
+    currentElement.onmouseenter = function(){
+        currentElement.style.backgroundColor = 'white'
     }
 }
-
-
-createGrid(3, 3)
